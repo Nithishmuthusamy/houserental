eb483ca0194914777139d9ffdceefd367b9c5c4b
Nithishmuthusamy <97595116+Nithishmuthusamy@users.noreply.github.com>
2022-08-17 11:39:32 +0530

    Update delete.js

--- app/controller/delete.js
+++ app/controller/delete.js
@@ -1,15 +1,3 @@
 import Controller from '@ember/controller';
-import { inject as service } from '@ember/service';
-import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 export default class DeleteController extends Controller {
-  @service('form-details') formDetail;
-  @tracked
-  id;
-  @action
-  delete(event) {
-    event.preventDefault();
-    console.log('deleted' + this.id);
-    this.formDetail.deleteDetails(this.id);
-  }
 }
