c2903e307b76f7435c7a6409ccf80d935b3823d3
Nithishmuthusamy <97595116+Nithishmuthusamy@users.noreply.github.com>
2022-08-17 09:25:54 +0530

    Update book.js

--- app/controller/book.js
+++ app/controller/book.js
@@ -15,38 +15,10 @@ export default class IndexBookController extends Controller {
   rent = 100;
   @tracked
   src;
-  // @action
-  // create(event) {
-  //   event.preventDefault();
-  //   console.log('this is controller');
-  //   this.next();
-  //   console.log('going in controller');
-  //   this.formDetail.addDetails(
-  //     this.number,
-  //     this.place,
-  //     this.no_of_guest,
-  //     this.rating,
-  //     this.rent,
-  //     this.src
-  //   );
-  //   ++this.number;
-  // }
   @service router;
   next() {
     this.router.transitionTo('index');
   }
-  //   forms = document.getElementsByClassName('needs-validation')
-
-  // for(this.forms in form){
-  // form.addEventListener( 'submit' , event => {
-  //   if (!form.checkValidity()) {
-  //     event.preventDefault()
-  //     event.stopPropagation()
-  //   }
-
-  //   form.classList.add('was-validated')
-  // })
-  // }
   @action
   submit(event) {
     event.preventDefault();
