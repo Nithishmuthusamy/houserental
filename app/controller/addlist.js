c5762f60c316fcebf24702156115fe0827b21a30
Nithishmuthusamy <97595116+Nithishmuthusamy@users.noreply.github.com>
2022-08-17 09:26:35 +0530

    Create addlist.js

---
+++ app/controller/addlist.js
@@ -0,0 +1,45 @@
+import Controller from '@ember/controller';
+import { service } from '@ember/service';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+export default class AddlistController extends Controller {
+  //@service('form-details') formDetail;
+  @service store;
+  @tracked
+  number = 4;
+  @tracked
+  place = 'New Delhi';
+  @tracked
+  no_of_guest = 1;
+  @tracked
+  rating = 5;
+  @tracked
+  rent = 100;
+  @tracked
+  src;
+  @service router;
+  next() {
+    this.router.transitionTo('index');
+  }
+  @action
+  create(event) {
+    event.preventDefault();
+    this.store.push({
+      data: [
+        {
+          id: this.number,
+          type: 'house',
+          attributes: {
+            src: this.src,
+            number: this.number,
+            place: this.place,
+            rating: this.rating,
+            rent: '₹' + this.rent,
+          },
+        },
+      ],
+    });
+    this.number++;
+    this.next();
+  }
+}
